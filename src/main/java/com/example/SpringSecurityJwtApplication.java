63374f252d8d4b7b3768deedf7c330f9b851de53
Jose Luis Martinez <chincola31122018@gmail.com>
2023-07-23 11:32:43 -0600

    first commit

---
+++ src/main/java/com/example/SpringSecurityJwtApplication.java
@@ -0,0 +1,70 @@
+package com.example;
+
+import com.example.models.ERole;
+import com.example.models.RoleEntity;
+import com.example.models.UserEntity;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Bean
+	CommandLineRunner init(){
+		return args -> {
+			UserEntity userEntity = UserEntity.builder()
+					.email("jose@gmail.com")
+					.username("jose")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.ADMIN.name()))
+							.build()))
+					.build();
+
+
+
+			UserEntity userEntity2 = UserEntity.builder()
+					.email("ale@gmail.com")
+					.username("ale")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.USER.name()))
+							.build()))
+					.build();
+
+
+
+			UserEntity userEntity3 = UserEntity.builder()
+					.email("carmen@gmail.com")
+					.username("carmen")
+					.password(passwordEncoder.encode("1234"))
+					.roles(Set.of(RoleEntity.builder()
+							.name(ERole.valueOf(ERole.INVITED.name()))
+							.build()))
+					.build();
+
+			userRepository.save(userEntity);
+			userRepository.save(userEntity2);
+			userRepository.save(userEntity3);
+
+		};
+	}
+}
